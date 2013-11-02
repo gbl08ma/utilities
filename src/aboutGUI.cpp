4da9074f9cc9892ce710948ec537866b475d7689
gbl08ma <gbl08ma@gmail.com>
2013-11-02 14:13:13 +0000

    Turn part of the about screen into a text area. Reduces the binary size by 0,5 KB.

--- src/aboutGUI.cpp
+++ src/aboutGUI.cpp
@@ -13,103 +13,76 @@
 #include <math.h>
 
 #include "aboutGUI.hpp"
+#include "textGUI.hpp"
 #include "versionProvider.hpp"
 #include "graphicsProvider.hpp" 
 #include "hardwareProvider.hpp" 
 
 void showAbout() {
- int key;
- Bdisp_AllClr_VRAM();
- Bdisp_EnableColor(1); 
- DefineStatusMessage((char*)"About Utilities", 1, 0, 0);
- DisplayStatusArea();
- //y increment between each line: 17; between paragraphs: 20
- int orange = drawRGB24toRGB565(210, 68, 19);
- int textX = 0;
- int textY = 5;
- char verBuffer[100] = "";
- getVersion(verBuffer);
- PrintMini(&textX, &textY, (unsigned char*)"Version ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLUE, COLOR_WHITE, 1, 0);
- PrintMini(&textX, &textY, (unsigned char*)verBuffer, 0, 0xFFFFFFFF, 0, 0, COLOR_BLUE, COLOR_WHITE, 1, 0);
- textY = textY + 17; textX = 0;
- getTimestamp(verBuffer);
- PrintMini(&textX, &textY, (unsigned char*)verBuffer, 0, 0xFFFFFFFF, 0, 0, COLOR_GRAY, COLOR_WHITE, 1, 0);
- textY = 42;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"Developed by gbl08ma at", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- drawtnyimLogo(10, 59+24, 0); //24 pixels for the status bar
- textY = 101;
- textX = 0;
- // PrintMini and its x,y pointers allow for awesome easy color formatting... let's try
- PrintMini(&textX, &textY, (unsigned char*)"tny. ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- PrintMini(&textX, &textY, (unsigned char*)"i", 0, 0xFFFFFFFF, 0, 0, orange, COLOR_WHITE, 1, 0);
- PrintMini(&textX, &textY, (unsigned char*)"nternet ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- PrintMini(&textX, &textY, (unsigned char*)"m", 0, 0xFFFFFFFF, 0, 0, orange, COLOR_WHITE, 1, 0);
- PrintMini(&textX, &textY, (unsigned char*)"edia", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 125;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"http://i.tny.im | http://gbl08ma.com", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 142;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"gbl08ma@gmail.com", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- 
- textX = 0; textY = 175;
- PrintMini(&textX, &textY, (unsigned char*)"Press any key", 0, 0xFFFFFFFF, 0, 0, COLOR_WHITE, COLOR_BLACK, 1, 0);
- GetKey(&key);
+  int key;
+  Bdisp_AllClr_VRAM();
+  Bdisp_EnableColor(1); 
+  DefineStatusMessage((char*)"About Utilities", 1, 0, 0);
+  DisplayStatusArea();
+  //y increment between each line: 17; between paragraphs: 20
+  int orange = drawRGB24toRGB565(210, 68, 19);
+  int textX = 0;
+  int textY = 5;
+  char verBuffer[100] = "";
+  getVersion(verBuffer);
+  PrintMini(&textX, &textY, (unsigned char*)"Version ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLUE, COLOR_WHITE, 1, 0);
+  PrintMini(&textX, &textY, (unsigned char*)verBuffer, 0, 0xFFFFFFFF, 0, 0, COLOR_BLUE, COLOR_WHITE, 1, 0);
+  textY = textY + 17; textX = 0;
+  getTimestamp(verBuffer);
+  PrintMini(&textX, &textY, (unsigned char*)verBuffer, 0, 0xFFFFFFFF, 0, 0, COLOR_GRAY, COLOR_WHITE, 1, 0);
+  textY = 42;
+  textX = 0;
+  PrintMini(&textX, &textY, (unsigned char*)"Developed by gbl08ma at", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  drawtnyimLogo(10, 59+24, 0); //24 pixels for the status bar
+  textY = 101;
+  textX = 0;
+  // PrintMini and its x,y pointers allow for awesome easy color formatting... let's try
+  PrintMini(&textX, &textY, (unsigned char*)"tny. ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  PrintMini(&textX, &textY, (unsigned char*)"i", 0, 0xFFFFFFFF, 0, 0, orange, COLOR_WHITE, 1, 0);
+  PrintMini(&textX, &textY, (unsigned char*)"nternet ", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  PrintMini(&textX, &textY, (unsigned char*)"m", 0, 0xFFFFFFFF, 0, 0, orange, COLOR_WHITE, 1, 0);
+  PrintMini(&textX, &textY, (unsigned char*)"edia", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  textY = 125;
+  textX = 0;
+  PrintMini(&textX, &textY, (unsigned char*)"http://i.tny.im | http://gbl08ma.com", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  textY = 142;
+  textX = 0;
+  PrintMini(&textX, &textY, (unsigned char*)"gbl08ma@gmail.com", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  
+  textX = 0; textY = 175;
+  PrintMini(&textX, &textY, (unsigned char*)"Press any key", 0, 0xFFFFFFFF, 0, 0, COLOR_WHITE, COLOR_BLACK, 1, 0);
+  GetKey(&key);
 
- Bdisp_AllClr_VRAM();
- Bdisp_EnableColor(1);
- DisplayStatusArea();
- textY = 5;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"Contains code by AHelper, merthsoft", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 22;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"and KermMartian at Cemetech", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 39;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"(http://cemetech.net), code by Simon", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 56;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"Lothar (http://casiopeia.net) and", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 73;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"public domain code by C.B. Falconer.", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 101;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"This Utilities add-in is licensed", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 118;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"under the GNU GPL v2, or at your", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 135;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"choice, any later version.", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+  Bdisp_AllClr_VRAM();
+  DisplayStatusArea();
+  
+  textArea text;
+  strcpy(text.title, (char*)"aa");
+  text.showtitle=0;
 
- textX = 0; textY = 175;
- PrintMini(&textX, &textY, (unsigned char*)"Press any key", 0, 0xFFFFFFFF, 0, 0, COLOR_WHITE, COLOR_BLACK, 1, 0);
- GetKey(&key);
-
- Bdisp_AllClr_VRAM();
- Bdisp_EnableColor(1);
- DisplayStatusArea();
- textY = 5;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"USE AT YOUR OWN RISK!", 0, 0xFFFFFFFF, 0, 0, COLOR_RED, COLOR_WHITE, 1, 0);
- textY = 22;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"PROVIDED WITHOUT WARRANTY!", 0, 0xFFFFFFFF, 0, 0, COLOR_RED, COLOR_WHITE, 1, 0);
- textY = 52;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"In no event will the authors be held", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 69;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"liable for any damages arising from", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
- textY = 86;
- textX = 0;
- PrintMini(&textX, &textY, (unsigned char*)"the use of this software.", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
-
- textX = 0; textY = 175;
- PrintMini(&textX, &textY, (unsigned char*)"Press any key", 0, 0xFFFFFFFF, 0, 0, COLOR_WHITE, COLOR_BLACK, 1, 0);
- GetKey(&key);
- DefineStatusMessage((char*)"", 1, 0, 0);
+  textElement elem[5];
+  text.elements = elem;
+  
+  elem[0].text = (char*)"Contains code by AHelper, merthsoft and KermMartian at Cemetech (http://cemetech.net), code by Simon Lothar (http://casiopeia.net) and public domain code by C.B. Falconer.";
+  elem[1].newLine = 1;
+  elem[1].lineSpacing = 8;
+  elem[1].text = (char*)"This Utilities add-in is licensed under the GNU GPL v2, or (at your option) any later version.";
+  elem[2].newLine = 1;
+  elem[2].lineSpacing = 8;
+  elem[2].color = COLOR_RED;
+  elem[2].text = (char*)"USE AT YOUR OWN RISK!";
+  elem[3].newLine = 1;
+  elem[3].color = COLOR_RED;
+  elem[3].text = (char*)"PROVIDED WITHOUT WARRANTY!";
+  elem[4].newLine = 1;
+  elem[4].lineSpacing = 8;
+  elem[4].text = (char*)"In no event will the authors be held liable for any damages arising from the use of this software.";
+  text.numelements = 5;
+  doTextArea(&text);
+  DefineStatusMessage((char*)"", 1, 0, 0);
 }
