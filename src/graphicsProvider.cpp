cca92f23f429e7eb5eaf8ee8cbfffcd81d869a6f
gbl08ma <gbl08ma@gmail.com>
2013-11-25 17:10:09 +0000

    graphicsProvider: add an inline implementation of the plot function. Amazingly enough, this reduced the file size a bit, instead of increasing. That's why I'm keeping it, even if we don't need the added speed of having an inline function.

--- src/graphicsProvider.cpp
+++ src/graphicsProvider.cpp
@@ -50,6 +50,13 @@ void plot(int x0, int y0, unsigned short color) {
   return; 
 }
 
+inline static void iplot(int x0, int y0, unsigned short color) {
+  unsigned short* VRAM = (unsigned short*)0xA8000000; 
+  VRAM += (y0*LCD_WIDTH_PX + x0); 
+  *VRAM = color;
+  return; 
+}
+
 void drawRectangle(int x, int y, int width, int height, unsigned short color) {
   unsigned short*VRAM = (unsigned short*)0xA8000000;
   for(int j = y; j < y+height; j++) {
@@ -67,7 +74,7 @@ void drawLine(int x1, int y1, int x2, int y2, int color) {
     int delta_x = (x2 > x1?(ix = 1, x2 - x1):(ix = -1, x1 - x2)) << 1; 
     int delta_y = (y2 > y1?(iy = 1, y2 - y1):(iy = -1, y1 - y2)) << 1; 
   
-    plot(x1, y1, color);  
+    iplot(x1, y1, color);  
     if (delta_x >= delta_y) { 
         int error = delta_y - (delta_x >> 1);        // error may go below zero 
         while (x1 != x2) { 
@@ -79,7 +86,7 @@ void drawLine(int x1, int y1, int x2, int y2, int color) {
          }                              // else do nothing 
             x1 += ix; 
             error += delta_y; 
-            plot(x1, y1, color); 
+            iplot(x1, y1, color); 
         } 
     } else { 
         int error = delta_x - (delta_y >> 1);      // error may go below zero 
@@ -92,7 +99,7 @@ void drawLine(int x1, int y1, int x2, int y2, int color) {
             }                              // else do nothing 
             y1 += iy; 
             error += delta_x;  
-            plot(x1, y1, color); 
+            iplot(x1, y1, color); 
         } 
     } 
 }
