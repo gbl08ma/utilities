45b60fb9ec41f2deb02e824dbb4aaee6e6f14122
gbl08ma <gbl08ma@gmail.com>
2014-01-23 21:51:12 +0000

    toolsGUI: improvements to user info reader.

--- src/toolsGUI.cpp
+++ src/toolsGUI.cpp
@@ -361,7 +361,7 @@ void systemInfo() {
   textArea text;
   strcpy(text.title, (char*)"System Information");
   
-  textElement elem[512];
+  textElement elem[10240];
   text.elements = elem;
   
   elem[0].text = (char*)"Operating System:";
@@ -440,31 +440,48 @@ void systemInfo() {
 
   text.numelements = 25;
 
+  char* flagpointer = (char*)0x80BE0000;
   char* orgpointer = (char*)0x80BE0004;
   char* namepointer = (char*)0x80BE0018;
   char* pwdpointer = (char*)0x80BE002C;
   int counter = 0;
-  while(text.numelements < 512) {
-    if(*orgpointer == '\0') break;
+  while(text.numelements < 10240) {
+    if(*flagpointer != 0x0F) break;
     elem[text.numelements].lineSpacing = 5;
     elem[text.numelements].newLine = 1;
     elem[text.numelements].text = (char*)"Username:";
     elem[text.numelements].spaceAtEnd=1;
     text.numelements++;
-    elem[text.numelements].text = namepointer;
+    if(*namepointer != '\0') {
+      elem[text.numelements].text = namepointer;
+    } else {
+      elem[text.numelements].text = (char*)"[Empty]";
+      elem[text.numelements].color = COLOR_LIGHTGRAY;
+    }
     text.numelements++;
     elem[text.numelements].newLine = 1;
     elem[text.numelements].text = (char*)"Organization:";
     elem[text.numelements].spaceAtEnd=1;
     text.numelements++;
-    elem[text.numelements].text = orgpointer;
+    if(*orgpointer != '\0') {
+      elem[text.numelements].text = orgpointer;
+    } else {
+      elem[text.numelements].text = (char*)"[Empty]";
+      elem[text.numelements].color = COLOR_LIGHTGRAY;
+    }
     text.numelements++;
     elem[text.numelements].newLine = 1;
     elem[text.numelements].text = (char*)"Password:";
     elem[text.numelements].spaceAtEnd=1;
     text.numelements++;
-    elem[text.numelements].text = pwdpointer;
+    if(*pwdpointer != '\0') {
+      elem[text.numelements].text = pwdpointer;
+    } else {
+      elem[text.numelements].text = (char*)"[Empty]";
+      elem[text.numelements].color = COLOR_LIGHTGRAY;
+    }
     text.numelements++;
+    flagpointer = flagpointer + 0x40;
     orgpointer = orgpointer + 0x40;
     namepointer = namepointer + 0x40;
     pwdpointer = pwdpointer + 0x40;
