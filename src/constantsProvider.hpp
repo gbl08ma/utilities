dc3bb5c468121034a1fa95f903aa42f0e1d1ef6c
gbl08ma <gbl08ma@gmail.com>
2014-03-21 14:41:06 +0000

    constantsProvider: update timestamps.

--- src/constantsProvider.hpp
+++ src/constantsProvider.hpp
@@ -31,9 +31,9 @@
 #endif
 
 #ifndef KNOWN_PAST_TIMESTAMP
-#define KNOWN_PAST_TIMESTAMP 1388966400*1000 // a timestamp that's known to be in the past for all timezones, so we can detect a unadjusted RTC
+#define KNOWN_PAST_TIMESTAMP 1395273600*1000 // a timestamp that's known to be in the past for all timezones, so we can detect a unadjusted RTC
 #endif
 
 #ifndef BUILD_EXPIRE_TIMESTAMP
-#define BUILD_EXPIRE_TIMESTAMP 1397516400*1000 // after the calc RTC is in a timestamp past this, users will be prompted to check for updates
+#define BUILD_EXPIRE_TIMESTAMP 1403222400*1000 // after the calc RTC is in a timestamp past this, users will be prompted to check for updates
 #endif
