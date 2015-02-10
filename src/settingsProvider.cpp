2ccaa1f789814e450e1fdcf0de547b25200fe8b1
gbl08ma <gbl08ma@gmail.com>
2015-02-10 18:12:45 +0000

    settingsProvider: simplify settings system a lot

--- src/settingsProvider.cpp
+++ src/settingsProvider.cpp
@@ -15,237 +15,24 @@
 #include "settingsProvider.hpp"
 #include "constantsProvider.hpp"
 
-#define SETTINGSFILE_VERSION 13 // NOTE: update this when changing the amount or meaning of settings!
-static int setting_self_fileversion = SETTINGSFILE_VERSION; // this is a special setting
-static int setting_timeformat = 0; // 0 = 24-hour HH:MM:SS ; 1 = 12-hour HH:MM:SS AM/PM
-static int setting_longdateformat = 0;
-/* 0 = "Weekday, Month 12"&mini 4-digits year below
-   1 = "Weekday, 12th Month"&mini 4-digits year below
-   2 = "Weekday, 12th Month 2121" (year not mini)
-   3 = "Weekday, 12th Month" (no year)
-   4 = "Month 12"&mini 4-digits year below
-   5 = "Month 12, 2012" (year not mini)
-   6 = "Month 12" (no year)
-   7 = "12th Month"&mini 4-digits year below
-   8 = "12th Month 2012" (year not mini)
-   9 = "12th Month" (no year)
-*/
-static int setting_dateformat = 0;
-/* 0 = DD/MM/YYYY
-   1 = MM/DD/YYYY
-   2 = YYYY/MM/DD
-*/
-static int setting_theme = 0; //whether the home screen (and later, possibly other things too) should use a dark/inverted theme. 0: white theme; 1: dark theme
-static int setting_display_statusbar = 1; //whether the status bar should be displayed
-static int setting_startup_brightness = 250; //screen brightness level to force on add-in startup, 0~249. 250 for no forcing //NOTE: was unsigned int before... why?
-static int setting_show_advanced = 0; //whether to show advanced things like CPU clock selection
-static int setting_display_fkeys = 1;
-static int setting_password_privacy = 1; // 0: show last input character when entering passwords; 1: do not show last char NOTE: this had a different name before and the meaning of the value is now different!
-static int setting_show_calendar_events_count = 1; // had a different name before, value meaning is the same
-static int setting_is_first_run = 1; // 1 if this is the first time the add-in is run, then it's set to zero and never changed, unless the user cancels update checking, in which case it's set to 2. So: if 1 is first run, if any other value is not first run.
-static int setting_enable_lock = 1; //whether lock functionality is available or not. Should always be on and not changeable on the settings menu, except when a specific code is entered on master-control in order to disable it to make people who don't want the lock, because they accidentally lock their calculators, happier (I'm thinking of you Catarina...)
-static int setting_lock_autooff = 0; //whether to turn off automatically after locking the calc
-static int setting_lock_on_exe = 0; //when enabled, calculator is locked when EXE is pressed on the home screen (i.e. legacy support for people used to the old lock add-in).
-static int setting_unlock_runmat = 0; //whether to jump to Run-Mat when calculator is unlocked. 2 when user should be asked.
-static int setting_clock_type = 0;
-/* what clock to show on home screen.
-   0 = digital time and long date - "traditional", like versions up to 1.0
-   1 = just digital time
-   2 = just digital time and short date
-   3 = just long date
-   4 = just short date
-   5 = analog clock, nothing else
-   6 = analog clock with digital time
-   7 = analog clock with d. time and long date
-   8 = analog clock with d. time and short date
-   9 = analog clock with long date
-   10 = analog clock with short date
-   11 = show nothing at all
-*/
-static int setting_clock_seconds = 1; // whether to show seconds in clock
-static int setting_default_calendar_view = 1; // default calendar view. 0 = week, 1 = month
-static int setting_week_start_day = 0; // 0 = sunday, 1 = monday
-static int setting_show_calendar_busy_map = 1;
-static int setting_chrono_notification_type = 1; // notification for when a downwards chrono finishes. 0: no notification; 1: pop-up with screen flashing; 2: pop-up without screen flashing; 3: message on home screen without pop-up
-static int setting_file_manager_sort = 1; // file list sort in file manager. 0: no sort (list as they appear), 1: A-Z, 2: Z-A, 3: size (ascending), 4: size (descending), 5: type A-Z, 6: type Z-A
-static int setting_file_manager_search = 1; // settings for search in the file manager; the value of this int is never used as a whole, but its bits are set individually for each search option.
-static int setting_timezone = 51; // clock offset from UTC in intervals of 15 min. Used in the TOTP authenticator. The value 0 represents UTC-12:45. 51 is UTC+00:00.
-
-/* home pane screens and shortcuts:
-   0 is disabled
-   1 is today events pane
-*/
-static int setting_home_pane_top = 0;
-static int setting_home_pane_right = 0;
-static int setting_home_pane_bottom = 0;
-static int setting_home_pane_left = 0;
+static unsigned char settings[NUMBER_OF_SETTINGS+1];
 
 // Routines for accessing and setting settings
 // NOTE: directly accessing setting_* variables is now strictly forbidden!
 
 int GetSetting(int setting) {
-  switch(setting) {
-    case SETTING_SELF_FILEVERSION:
-      return setting_self_fileversion;
-    case SETTING_TIMEFORMAT:
-      return setting_timeformat;
-    case SETTING_LONGDATEFORMAT:
-      return setting_longdateformat;
-    case SETTING_DATEFORMAT:
-      return setting_dateformat;
-    case SETTING_THEME:
-      return setting_theme;
-    case SETTING_DISPLAY_STATUSBAR:
-      return setting_display_statusbar;
-    case SETTING_STARTUP_BRIGHTNESS:
-      return setting_startup_brightness;
-    case SETTING_SHOW_ADVANCED:
-      return setting_show_advanced;
-    case SETTING_DISPLAY_FKEYS:
-      return setting_display_fkeys;
-    case SETTING_PASSWORD_PRIVACY:
-      return setting_password_privacy;
-    case SETTING_SHOW_CALENDAR_EVENTS_COUNT:
-      return setting_show_calendar_events_count;
-    case SETTING_IS_FIRST_RUN:
-      return setting_is_first_run;
-    case SETTING_ENABLE_LOCK:
-      return setting_enable_lock;
-    case SETTING_LOCK_AUTOOFF:
-      return setting_lock_autooff;
-    case SETTING_LOCK_ON_EXE:
-      return setting_lock_on_exe;
-    case SETTING_UNLOCK_RUNMAT:
-      return setting_unlock_runmat;
-    case SETTING_CLOCK_TYPE:
-      return setting_clock_type;
-    case SETTING_CLOCK_SECONDS:
-      return setting_clock_seconds;
-    case SETTING_DEFAULT_CALENDAR_VIEW:
-      return setting_default_calendar_view;
-    case SETTING_WEEK_START_DAY:
-      return setting_week_start_day;
-    case SETTING_SHOW_CALENDAR_BUSY_MAP:
-      return setting_show_calendar_busy_map;
-    case SETTING_CHRONO_NOTIFICATION_TYPE:
-      return setting_chrono_notification_type;
-    case SETTING_FILE_MANAGER_SORT:
-      return setting_file_manager_sort;
-    case SETTING_FILE_MANAGER_SEARCH:
-      return setting_file_manager_search;
-    case SETTING_TIMEZONE:
-      return setting_timezone;
-    case SETTING_HOME_PANE_TOP:
-      return setting_home_pane_top;
-    case SETTING_HOME_PANE_RIGHT:
-      return setting_home_pane_right;
-    case SETTING_HOME_PANE_BOTTOM:
-      return setting_home_pane_bottom;
-    case SETTING_HOME_PANE_LEFT:
-      return setting_home_pane_left;
-    default:
-      return 0;
-  }
+  return settings[setting];
 }
 
 void SetSetting(int setting, int value, int autosave) {
-  switch(setting) {
-    case SETTING_SELF_FILEVERSION:
-      // do nothing. this "setting" is not meant to be changed at runtime.
-      break;
-    case SETTING_TIMEFORMAT:
-      setting_timeformat = value;
-      break;
-    case SETTING_LONGDATEFORMAT:
-      setting_longdateformat = value;
-      break;
-    case SETTING_DATEFORMAT:
-      setting_dateformat = value;
-      break;
-    case SETTING_THEME:
-      setting_theme = value;
-      break;
-    case SETTING_DISPLAY_STATUSBAR:
-      setting_display_statusbar = value;
-      // This automatically sets the statusbar to show or hide throughout all the UI:
-      if (setting_display_statusbar) {
-        EnableStatusArea(0);
-        DefineStatusAreaFlags(3, SAF_BATTERY | SAF_TEXT | SAF_GLYPH | SAF_ALPHA_SHIFT, 0, 0);
-      } else {
-        EnableStatusArea(3);
-      }
-      break;
-    case SETTING_STARTUP_BRIGHTNESS:
-      setting_startup_brightness = value;
-      break;
-    case SETTING_SHOW_ADVANCED:
-      setting_show_advanced = value;
-      break;
-    case SETTING_DISPLAY_FKEYS:
-      setting_display_fkeys = value;
-      break;
-    case SETTING_PASSWORD_PRIVACY:
-      setting_password_privacy = value;
-      break;
-    case SETTING_SHOW_CALENDAR_EVENTS_COUNT:
-      setting_show_calendar_events_count = value;
-      break;
-    case SETTING_IS_FIRST_RUN:
-      setting_is_first_run = value;
-      break;
-    case SETTING_ENABLE_LOCK:
-      setting_enable_lock = value;
-      break;
-    case SETTING_LOCK_AUTOOFF:
-      setting_lock_autooff = value;
-      break;
-    case SETTING_LOCK_ON_EXE:
-      setting_lock_on_exe = value;
-      break;
-    case SETTING_UNLOCK_RUNMAT:
-      setting_unlock_runmat = value;
-      break;
-    case SETTING_CLOCK_TYPE:
-      setting_clock_type = value;
-      break;
-    case SETTING_CLOCK_SECONDS:
-      setting_clock_seconds = value;
-      break;
-    case SETTING_DEFAULT_CALENDAR_VIEW:
-      setting_default_calendar_view = value;
-      break;
-    case SETTING_WEEK_START_DAY:
-      setting_week_start_day = value;
-      break;
-    case SETTING_SHOW_CALENDAR_BUSY_MAP:
-      setting_show_calendar_busy_map = value;
-      break;
-    case SETTING_CHRONO_NOTIFICATION_TYPE:
-      setting_chrono_notification_type = value;
-      break;
-    case SETTING_FILE_MANAGER_SORT:
-      setting_file_manager_sort = value;
-      break;
-    case SETTING_FILE_MANAGER_SEARCH:
-      setting_file_manager_search = value;
-      break;
-    case SETTING_TIMEZONE:
-      setting_timezone = value;
-      break;
-    case SETTING_HOME_PANE_TOP:
-      setting_home_pane_top = value;
-      break;
-    case SETTING_HOME_PANE_RIGHT:
-      setting_home_pane_right = value;
-      break;
-    case SETTING_HOME_PANE_BOTTOM:
-      setting_home_pane_bottom = value;
-      break;
-    case SETTING_HOME_PANE_LEFT:
-      setting_home_pane_left = value;
-      break;
-    default:
-      break;
+  settings[setting] = value;
+  if(setting == SETTING_DISPLAY_STATUSBAR) {
+    if(settings[setting]) {
+      EnableStatusArea(0);
+      DefineStatusAreaFlags(3, SAF_BATTERY | SAF_TEXT | SAF_GLYPH | SAF_ALPHA_SHIFT, 0, 0);
+    } else {
+      EnableStatusArea(3);
+    }
   }
   if (autosave) SaveSettings();
 }
@@ -254,21 +41,34 @@ void SetSetting(int setting, int value, int autosave) {
 int LoadSettings() { // returns 0 on success, 1 if settings were reset (first run / incompatibility)
   int size;
   MCSGetDlen2(DIRNAME, SETTINGSFILE, &size);
-  if (size <= 16) return 1; // no settings file or incompatible, reset settings.
+  if (size <= 16) goto resetSettings; // no settings file or incompatible, reset settings.
 
-  unsigned char buffer[NUMBER_OF_SETTINGS+2];
-  MCSGetData1(0, NUMBER_OF_SETTINGS+1, buffer); // +1 because NUMBER_OF_SETTINGS does not include the file version
+  MCSGetData1(0, NUMBER_OF_SETTINGS+1, settings); // +1 because NUMBER_OF_SETTINGS does not include the file version
   // detect if the current settings file version is compatible with the one we're expecting.
   // file version is always the first byte, except for the settings files of versions 1.0 and earlier.
   // for these, the filesize happened to be equal or smaller than 16 (checked above).
   // if we got here, the file is a "modern" one, so check the version byte.
-  if (buffer[0] != SETTINGSFILE_VERSION) return 1; //if incompatible, reset settings
-  
-  // setting count starts at one (zero is for the file version, which we already took care of)
-  // this assumes same setting IDs across files. Also, setting IDs must be consecutive
-  // still, if a setting has no correspondence, SetSetting will just ignore the command...
-  for(int curupd = 1; curupd <= NUMBER_OF_SETTINGS; curupd++) SetSetting(curupd, buffer[curupd], 0); // do not save every time, as we're loading right now...
+  if (settings[0] != SETTINGSFILE_VERSION) goto resetSettings; //if incompatible, reset settings
   return 0;
+
+  resetSettings:
+  for(int i = 1; i <= NUMBER_OF_SETTINGS; i++) settings[i] = 0;
+  // the defaults for some settings are not zero:
+  settings[SETTING_DISPLAY_STATUSBAR] = 1;
+  settings[SETTING_STARTUP_BRIGHTNESS] = 250;
+  settings[SETTING_DISPLAY_FKEYS] = 1;
+  settings[SETTING_PASSWORD_PRIVACY] = 1;
+  settings[SETTING_SHOW_CALENDAR_EVENTS_COUNT] = 1;
+  settings[SETTING_IS_FIRST_RUN] = 1;
+  settings[SETTING_ENABLE_LOCK] = 1;
+  settings[SETTING_CLOCK_SECONDS] = 1;
+  settings[SETTING_DEFAULT_CALENDAR_VIEW] = 1;
+  settings[SETTING_SHOW_CALENDAR_BUSY_MAP] = 1;
+  settings[SETTING_CHRONO_NOTIFICATION_TYPE] = 1;
+  settings[SETTING_FILE_MANAGER_SORT] = 1;
+  settings[SETTING_FILE_MANAGER_SEARCH] = 1;
+  settings[SETTING_TIMEZONE] = 51;
+  return 1;
 }
 
 void SaveSettings() {
@@ -276,11 +76,6 @@ void SaveSettings() {
   { // directory already exists, so delete the exiting file that may be there
     MCSDelVar2(DIRNAME, SETTINGSFILE);
   }
-  unsigned char buffer[NUMBER_OF_SETTINGS+2];
-  int curupd = 0; //setting count starts at one, but we want to retrieve the file version too...
-  while(curupd <= NUMBER_OF_SETTINGS+1) { //+1 because here we want to save the file version too
-    buffer[curupd] = GetSetting(curupd);
-    curupd++;
-  }
-  MCSPutVar2(DIRNAME, SETTINGSFILE, NUMBER_OF_SETTINGS+1, buffer); //+1 on purpose again, for the same reason
+  settings[SETTING_SELF_FILEVERSION] = SETTINGSFILE_VERSION;
+  MCSPutVar2(DIRNAME, SETTINGSFILE, NUMBER_OF_SETTINGS+1, settings); //+1 on purpose again, for the same reason
 }
--- src/settingsProvider.hpp
+++ src/settingsProvider.hpp
@@ -20,37 +20,71 @@
 // SETTINGS engine
 /*App settings.*/
 // the default value of the settings goes on settingsProvider.cpp
-#define SETTING_SELF_FILEVERSION 0
-#define SETTING_TIMEFORMAT 1
+
+#define SETTINGSFILE_VERSION 13 // NOTE: increase this when changing the amount or meaning of settings!
+
+#define SETTING_SELF_FILEVERSION 0 // this is a special field used by the settings system to determine file compatibility
+#define SETTING_TIMEFORMAT 1 // 0 = 24-hour HH:MM:SS ; 1 = 12-hour HH:MM:SS AM/PM
 #define SETTING_LONGDATEFORMAT 2
-#define SETTING_DATEFORMAT 3
-#define SETTING_THEME 4
-#define SETTING_DISPLAY_STATUSBAR 5
-#define SETTING_STARTUP_BRIGHTNESS 6
-#define SETTING_SHOW_ADVANCED 7
-#define SETTING_DISPLAY_FKEYS 8
-#define SETTING_PASSWORD_PRIVACY 9
-#define SETTING_SHOW_CALENDAR_EVENTS_COUNT 10
-#define SETTING_IS_FIRST_RUN 11
-#define SETTING_ENABLE_LOCK 12
-#define SETTING_LOCK_AUTOOFF 13
-#define SETTING_LOCK_ON_EXE 14
-#define SETTING_UNLOCK_RUNMAT 15
+/* 0 = "Weekday, Month 12"&mini 4-digits year below
+   1 = "Weekday, 12th Month"&mini 4-digits year below
+   2 = "Weekday, 12th Month 2121" (year not mini)
+   3 = "Weekday, 12th Month" (no year)
+   4 = "Month 12"&mini 4-digits year below
+   5 = "Month 12, 2012" (year not mini)
+   6 = "Month 12" (no year)
+   7 = "12th Month"&mini 4-digits year below
+   8 = "12th Month 2012" (year not mini)
+   9 = "12th Month" (no year)
+*/
+
+#define SETTING_DATEFORMAT 3 // 0 = DD/MM/YYYY, 1 = MM/DD/YYYY, 2 = YYYY/MM/DD
+#define SETTING_THEME 4  // whether the home screen should use a dark/inverted theme. 0: white theme; 1: dark theme
+#define SETTING_DISPLAY_STATUSBAR 5 // whether the status bar should be displayed
+#define SETTING_STARTUP_BRIGHTNESS 6 // screen brightness level to force on add-in startup, 0~249. 250 for no forcing
+#define SETTING_SHOW_ADVANCED 7  // whether to show advanced things like CPU clock selection
+#define SETTING_DISPLAY_FKEYS 8 // 0: show last input character when entering passwords; 1: do not show last char
+#define SETTING_PASSWORD_PRIVACY 9 // had a different name before, value meaning is the same
+#define SETTING_SHOW_CALENDAR_EVENTS_COUNT 10 // 1 if this is the first time the add-in is run, then it's set to zero and never changed, unless the user cancels update checking, in which case it's set to 2. So: if 1 is first run, if any other value is not first run.
+#define SETTING_IS_FIRST_RUN 11 // 1 if this is the first time the add-in is run, then it's set to zero and never changed, unless the user cancels update checking, in which case it's set to 2. So: if 1 is first run, if any other value is not first run.
+#define SETTING_ENABLE_LOCK 12  // whether lock functionality is available or not. Should always be on and not changeable on the settings menu, except when special instructions are given to disable the function
+#define SETTING_LOCK_AUTOOFF 13 // whether to turn off automatically after locking the calc
+#define SETTING_LOCK_ON_EXE 14 // when enabled, calculator is locked when EXE is pressed on the home screen (i.e. legacy support for people used to the old lock add-in).
+#define SETTING_UNLOCK_RUNMAT 15 // whether to jump to Run-Mat when calculator is unlocked. 2 when user should be asked.
 #define SETTING_CLOCK_TYPE 16
-#define SETTING_CLOCK_SECONDS 17
-#define SETTING_DEFAULT_CALENDAR_VIEW 18
-#define SETTING_WEEK_START_DAY 19
-#define SETTING_SHOW_CALENDAR_BUSY_MAP 20
-#define SETTING_CHRONO_NOTIFICATION_TYPE 21
-#define SETTING_FILE_MANAGER_SORT 22
-#define SETTING_FILE_MANAGER_SEARCH 23
-#define SETTING_TIMEZONE 24
+/* what clock to show on home screen.
+   0 = digital time and long date - "traditional", like versions up to 1.0
+   1 = just digital time
+   2 = just digital time and short date
+   3 = just long date
+   4 = just short date
+   5 = analog clock, nothing else
+   6 = analog clock with digital time
+   7 = analog clock with d. time and long date
+   8 = analog clock with d. time and short date
+   9 = analog clock with long date
+   10 = analog clock with short date
+   11 = show nothing at all
+*/
+#define SETTING_CLOCK_SECONDS 17 // whether to show seconds in clock
+#define SETTING_DEFAULT_CALENDAR_VIEW 18 // default calendar view. 0 = week, 1 = month
+#define SETTING_WEEK_START_DAY 19 // 0 = sunday, 1 = monday
+#define SETTING_SHOW_CALENDAR_BUSY_MAP 20 
+#define SETTING_CHRONO_NOTIFICATION_TYPE 21 // notification for when a downwards chrono finishes. 0: no notification; 1: pop-up with screen flashing; 2: pop-up without screen flashing; 3: message on home screen without pop-up
+#define SETTING_FILE_MANAGER_SORT 22 // file list sort in file manager. 0: no sort (list as they appear), 1: A-Z, 2: Z-A, 3: size (ascending), 4: size (descending), 5: type A-Z, 6: type Z-A
+#define SETTING_FILE_MANAGER_SEARCH 23 // settings for search in the file manager; the value of this is never used as a whole, but its bits are set individually for each search option.
+#define SETTING_TIMEZONE 24 // clock offset from UTC in intervals of 15 min. Used in the TOTP authenticator. The value 0 represents UTC-12:45. 51 is UTC+00:00.
+
+/* home pane screens and shortcuts:
+   0 is disabled
+   1 is today events pane
+*/
 #define SETTING_HOME_PANE_TOP 25
 #define SETTING_HOME_PANE_RIGHT 26
 #define SETTING_HOME_PANE_BOTTOM 27
 #define SETTING_HOME_PANE_LEFT 28
 
-#define NUMBER_OF_SETTINGS 28 //NOTE update this when adding settings. DOES NOT include the SETTING_SELF_FILEVERSION
+#define NUMBER_OF_SETTINGS 28 // NOTE update this when adding settings. DOES NOT include the SETTING_SELF_FILEVERSION
 /*End of settings*/ 
 
 int GetSetting(int setting);
