376f1d52de3338a8a644db00b41eb543531c9ef7
gbl08ma <gbl08ma@gmail.com>
2016-06-21 13:35:59 +0100

    editorGUI: ask if file is to be saved before exiting

--- src/editorGUI.cpp
+++ src/editorGUI.cpp
@@ -20,6 +20,7 @@
 #include "graphicsProvider.hpp"
 #include "fileProvider.hpp"
 #include "fileGUI.hpp"
+#include "sha1.h"
 
 void textfileEditor(char* filename, char* basefolder) {
   int newfile = (filename == NULL);
@@ -50,69 +51,92 @@ void textfileEditor(char* filename, char* basefolder) {
   //input.forcetext=1;
   input.charlimit=TEXT_BUFFER_SIZE;
   input.buffer = (char*)sText;
+
+  // calculate checksum so we can check for changes
+  unsigned char origHash[20] = "";
+  sha1((unsigned char*)sText, strlen(sText), origHash);
   while(1) {
     input.key=0;
     int res = doTextEdit(&input);
-    if (res==TEXTEDIT_RETURN_EXIT) return; // user aborted
-    else if (res==TEXTEDIT_RETURN_CONFIRM) {
-      char newfilename[MAX_FILENAME_SIZE];
-      unsigned short newfilenameshort[0x10A];
-      if(newfile) {
-        int backToEditor = 0;
-        SetBackGround(13);
-        drawScreenTitle("Text Editor", "Save file as:");
-        drawFkeyLabels(0x036F); // <
-        textInput ninput;
-        ninput.forcetext=1;
-        ninput.charlimit=MAX_NAME_SIZE;
-        char nfilename[MAX_NAME_SIZE];
-        nfilename[0] = 0;
-        ninput.buffer = (char*)nfilename;
-        while(1) {
-          ninput.key = 0;
-          int nres = doTextInput(&ninput);
-          if (nres==INPUT_RETURN_EXIT || (nres==INPUT_RETURN_KEYCODE && ninput.key==KEY_CTRL_F1)) {
-            // user aborted
-            backToEditor = 1;
-            break;
-          } else if (nres==INPUT_RETURN_CONFIRM) {
-            if(stringEndsInG3A(nfilename)) {
-              mMsgBoxPush(4);
-              multiPrintXY(3, 2, "g3a files can't\nbe created by\nan add-in.",
-                           TEXT_MODE_TRANSPARENT_BACKGROUND, TEXT_COLOR_BLACK);
-              closeMsgBox();
-            } else {
-              // create and save file
-              strcpy(newfilename, basefolder);
-              strcat(newfilename, nfilename);
-              Bfile_StrToName_ncpy(newfilenameshort, newfilename, 0x10A);
-              break;
-            }
+    switch(res) {
+      case TEXTEDIT_RETURN_EXIT:
+      {
+        unsigned char newHash[20] = "";
+        sha1((unsigned char*)sText, strlen(sText), newHash);
+        if(!memcmp(origHash, newHash, 20)) return;
+        else {
+          mMsgBoxPush(4);
+          mPrintXY(3, 2, "Save this file?", TEXT_MODE_TRANSPARENT_BACKGROUND,
+                   TEXT_COLOR_BLACK);
+          if(closeMsgBox(1, 4)) {
+            // fall through
+          } else {
+            return;
           }
         }
-        if(backToEditor) continue;
-      } else {
-        // delete, then create and save file
-        Bfile_StrToName_ncpy(newfilenameshort, filename, 0x10A);
-        Bfile_DeleteEntry(newfilenameshort);
       }
-      size_t size = strlen(sText);
-      if(Bfile_CreateEntry_OS(newfilenameshort, CREATEMODE_FILE, &size) < 0) { //create the file
-        // it appears file exists, overwrite?
-        if(overwriteFilePrompt(newfilename)) {
+      case TEXTEDIT_RETURN_CONFIRM:
+      {
+        char newfilename[MAX_FILENAME_SIZE];
+        unsigned short newfilenameshort[0x10A];
+        if(newfile) {
+          int backToEditor = 0;
+          SetBackGround(13);
+          drawScreenTitle("Text Editor", "Save file as:");
+          drawFkeyLabels(0x036F); // <
+          textInput ninput;
+          ninput.forcetext=1;
+          ninput.charlimit=MAX_NAME_SIZE;
+          char nfilename[MAX_NAME_SIZE];
+          nfilename[0] = 0;
+          ninput.buffer = (char*)nfilename;
+          while(1) {
+            ninput.key = 0;
+            int nres = doTextInput(&ninput);
+            if (nres==INPUT_RETURN_EXIT || (nres==INPUT_RETURN_KEYCODE && ninput.key==KEY_CTRL_F1)) {
+              // user aborted
+              backToEditor = 1;
+              break;
+            } else if (nres==INPUT_RETURN_CONFIRM) {
+              if(stringEndsInG3A(nfilename)) {
+                mMsgBoxPush(4);
+                multiPrintXY(3, 2, "g3a files can't\nbe created by\nan add-in.",
+                             TEXT_MODE_TRANSPARENT_BACKGROUND, TEXT_COLOR_BLACK);
+                closeMsgBox();
+              } else {
+                // create and save file
+                strcpy(newfilename, basefolder);
+                strcat(newfilename, nfilename);
+                Bfile_StrToName_ncpy(newfilenameshort, newfilename, 0x10A);
+                break;
+              }
+            }
+          }
+          if(backToEditor) continue;
+        } else {
+          // delete, then create and save file
+          Bfile_StrToName_ncpy(newfilenameshort, filename, 0x10A);
           Bfile_DeleteEntry(newfilenameshort);
-          Bfile_CreateEntry_OS(newfilenameshort, CREATEMODE_FILE, &size);
         }
-        else continue; // abort file save so user can discard the file, or type another filename.
+        size_t size = strlen(sText);
+        if(Bfile_CreateEntry_OS(newfilenameshort, CREATEMODE_FILE, &size) < 0) { //create the file
+          // it appears file exists, overwrite?
+          if(overwriteFilePrompt(newfilename)) {
+            Bfile_DeleteEntry(newfilenameshort);
+            Bfile_CreateEntry_OS(newfilenameshort, CREATEMODE_FILE, &size);
+          }
+          else continue; // abort file save so user can discard the file, or type another filename.
+        }
+        
+        int h = Bfile_OpenFile_OS(newfilenameshort, READWRITE, 0);
+        if(h >= 0) { // Still failing?
+          //Write file contents
+          Bfile_WriteFile_OS(h, sText, size);
+          Bfile_CloseFile_OS(h);
+        }
+        return;
       }
-      
-      int h = Bfile_OpenFile_OS(newfilenameshort, READWRITE, 0);
-      if(h >= 0) { // Still failing?
-        //Write file contents
-        Bfile_WriteFile_OS(h, sText, size);
-        Bfile_CloseFile_OS(h);
-      }
-      return;
+      break;
     }
   }
 }
